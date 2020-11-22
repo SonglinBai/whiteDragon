e82d71cdf66cc37e64a89cb62f9a32b8b3f6f91d
zhangxinchen <814638168@qq.com>
2020-11-22 21:01:09 +0800

    transform attemp to str

--- domain/Node.py
+++ domain/Node.py
@@ -4,7 +4,7 @@ from domain.permission import *
 
 from domain.Attemp import *
 class Node(object):
-    def __init__(self,label: str,type:int, position:List,port:list,Attemp:List[Attemp]):#
+    def __init__(self,label: str,type:int, position:List,port:list,Attemp:List[str]):#
         self.label = label#编号
         self.type = type #0-主机 1-服务器
         self.position = position
